6b1f5af280c13b64d7ace8f85c9d925d0a732c0d
h-aqua25 <aqua.hara25@gmail.com>
2022-08-19 11:44:16 +0900

    deviseメイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get 'user/:id', to: 'users/registrations#show'
+    get 'signup', to: 'users/registrations#new'
+    get 'login', to: 'users/sessions#new'
+    get 'logout', to: 'users/sessions#destroy'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
