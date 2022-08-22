250b1532013decb0beeaadf3b66f2d66c1bfe21a
h-aqua25 <aqua.hara25@gmail.com>
2022-08-22 20:12:33 +0900

    deviseのrspec等の初期設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@
 
 FactoryBot.define do
   factory :user do
+    sequence(:name) { |n| "名前_#{n}" }
+    sequence(:email) { |n| "Eメール#{n}@gmail.com" }
+    sequence(:password) { |n| "password#{n}" }
+    sequence(:password_confirmation) { |n| "password#{n}" }
   end
 end
